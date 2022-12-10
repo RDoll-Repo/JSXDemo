f0c6b4f3bea0e15e0027be1615597501899a97c2
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2022-12-10 02:07:15 -0500

    props demo

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ function App() {
     let message = 'Bye there!';
     let name = 'Roland';
     let age = 26
+    const inputType = 'number';
 
     if(Math.random() > 0.5) {
         message = 'Hello there!';
@@ -23,6 +24,7 @@ function App() {
             <h1>{message}</h1>
             <h1>{new Date().toLocaleTimeString()}</h1>
             <h1>My name is {name}. I am {age} years old.</h1>
+            <input style={{border: '3px solid red'}} type={inputType} min={5} />
         </div>
     );
     //return <h1>{message}</h1>;
